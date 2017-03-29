0b5c2c944f616df75cf356a26322516418dbe105
JasonLadieu <jason.ladieu@gmail.com>
2017-03-28 21:16:54 -0400

    Update String Concatenation.py

--- String Concatenation.py
+++ String Concatenation.py
@@ -1,1 +1,9 @@
+#I want you to help me make a string to use for an email greeting. 
+#To start, we need a variable named name that's assigned to your name. Deja vu, huh?
 
+name = 'Jason'
+
+#Make a new variable named subject that concatenates (uses the + sign) the string "Treehouse loves" and your name variable.
+#You've probably received an email or two with this subject already!
+
+subject = "Treehouse loves " + name
