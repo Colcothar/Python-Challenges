97f9aa0681f030f0727dc6246dead78d54226353
JasonLadieu <jason.ladieu@gmail.com>
2017-03-28 21:26:05 -0400

    Create string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,9 @@
+#I want to make the email subject one more time, but this time let's use the .format() method for strings.
+#Go ahead and make your name variable again. Hey, all practice is good practice, right?
+
+name = "Jason"
+
+#OK, now use .format() on the string "Treehouse loves {}" to put your name into the placeholder. 
+#Assign this to the variable subject (so start with subject =).
+
+subject = "Treehouse loves {}".format(name)
