14410461dbe3ee5819be8c703a9e3db6bb14e83a
JasonLadieu <jason.ladieu@gmail.com>
2017-03-28 20:58:32 -0400

    Create Age_Challange.py

---
+++ Age_Challange.py
@@ -0,0 +1,4 @@
+# Let's calculate, with a bit of imprecision, how many days old you are.
+# First, create a variable named years. Assign years to the number of years old you are. This should be an integer. 
+
+years = 28
