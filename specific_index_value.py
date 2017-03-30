be521442ab1cbb88fe0e8cfd370c5fea96985eac
JasonLadieu <jason.ladieu@gmail.com>
2017-03-30 19:51:07 -0400

    Create specific_index_value.py

---
+++ specific_index_value.py
@@ -0,0 +1,9 @@
+#Make a new variable named var1 that is either a list or a string. 
+#Make sure that var1[5], the item with an index value of 5, is the letter "t".
+
+var1 = [1, 2, 3, 4, 5, 't']
+
+#Make a new variable, var2, with "e" at index -2. 
+#Remember, negative indexes start at the right side of the iterable.
+
+var2 = ['a', 'b', 'e', 'c']
