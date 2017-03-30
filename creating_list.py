71415d13c884b2877204a3d52dac6a0816b1b1a3
JasonLadieu <jason.ladieu@gmail.com>
2017-03-29 21:25:15 -0400

    Create creating_list.py

---
+++ creating_list.py
@@ -0,0 +1,4 @@
+#Make a new variable named colors. Assign it to a list of at least 5 items. 
+#They should be colors but I won't mark you off for that.
+
+colors = ['blue', 'magenta', 'pink', 'purple', 'red']
