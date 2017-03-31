758e96fbd6598d845dfe09cb5d3b8d0a2d75f624
JasonLadieu <jason.ladieu@gmail.com>
2017-03-30 20:12:29 -0400

    Create list_cleanup.py

---
+++ list_cleanup.py
@@ -0,0 +1,10 @@
+#Oh no! I have another messy list. This one has some even numbers in it and I want it to only have odd numbers.
+#Use del to delete the 8 from messy.
+
+messy = [5, 2, 8, 1, 3]
+
+del messy[2]
+
+#And, finally, we need to remove that 2. Can you delete it with del, too? Thanks!
+
+del messsy[1]
