7a57424d1f99dc95c834affb11da917ee546f724
JasonLadieu <jason.ladieu@gmail.com>
2017-03-30 20:46:52 -0400

    Create conditional_value.py

---
+++ conditional_value.py
@@ -0,0 +1,17 @@
+#I'm going to create a variable named age.
+#I need you to make an if condition that sets admitted to True if age is 13 or more.
+
+admitted = None
+
+if age >= 13:
+    admitted = True
+
+    
+#OK, one more. Add an else to your if. In the else, set admitted to False.
+
+admitted = None
+
+if age >= 13:
+  admitted = True
+else:
+  admitted = False
