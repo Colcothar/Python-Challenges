35a40727161f656c777ec04044fe707fa09860ff
JasonLadieu <jason.ladieu@gmail.com>
2017-03-31 20:03:16 -0400

    Create use_input.py

---
+++ use_input.py
@@ -0,0 +1,5 @@
+#Simple one this time. Make a variable named age that is assigned to an input() call. 
+#Give your input() a prompt of "What is your age?". 
+#You might need to add a space in there, though.
+
+age = int(input("What is your age? "))
