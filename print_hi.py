a4440ee1879706d66c053d6c9c83198cc32beb37
JasonLadieu <jason.ladieu@gmail.com>
2017-04-02 20:23:01 -0400

    Update print_hi.py

--- print_hi.py
+++ print_hi.py
@@ -1,4 +1,4 @@
-Write a function named printer. 
+#Write a function named printer. 
 #The function should take a single argument, count, and should print "Hi " as many times as the count argument. 
 #Remember, you can multiply a string by an integer.
 
