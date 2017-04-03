c08767c98d818db255de338a2cce51c44432987a
JasonLadieu <jason.ladieu@gmail.com>
2017-04-02 20:25:02 -0400

    Create function_product.py

---
+++ function_product.py
@@ -0,0 +1,6 @@
+#I need you to write a function named product. 
+#It should take two arguments, you can call them whatever you want, and then multiply them together and return the result.
+
+def product(num1, num2):
+    return num1 * num2
+    
